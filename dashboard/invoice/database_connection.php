a6f49183476520e7b2b0dd12c85ec4526891bdac
mohamedndiayedev <jaayahmad@gmail.com>
2020-08-07 01:48:37 +0100

    updates

--- dashboard/invoice/database_connection.php
+++ dashboard/invoice/database_connection.php
@@ -1,3 +1,3 @@
 <?php
-$connect = new PDO('mysql:host=localhost;dbname=invoice', 'root', '');
+$connect = mysqli_connect("iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","ybohxgtnw8ag3b1l","xtf6joq3n6czftrh","v1ydnphp6nxbbqix");
 ?>
